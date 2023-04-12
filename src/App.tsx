3cfba6a4ea9f53b21914d4e5d0fc7124a20e0fe0
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-04-12 19:33:32 +0800

    typescript practice with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import TodoList from './Components/TodoList';
+import NewTodo from './Components/NewTodo';
+import { Todo } from './TodoModel';
+
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prev) => [...prev, { id: Math.random().toString(), text: text }]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== todoId));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteHandler={todoDeleteHandler} />
     </div>
   );
-}
+};
 
 export default App;
